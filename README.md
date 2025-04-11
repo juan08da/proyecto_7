0aa20ed0395b62150990e1b83240c0ac8a4519b3
juan08da <felipe.ramirezb98@gmail.com>
2025-04-10 21:52:25 -0500

    actualizacion

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # proyecto_7
+Esta aplicación nos permite crear un histograma y un gráfico de dispersión para un conjunto de datos de vehiculos
--- app.py
+++ app.py
@@ -16,5 +16,17 @@ if hist_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
-    
+
+dispersion_button = st.button('Construir un gráfico de dispersión') # crear un botón
+if dispersion_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+        
+    # mostrar un gráfico Plotly interactivo
+    fig.show()
+
+
     
